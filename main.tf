f52c64ea239aaec05d21f541cf3a9e049ad8e23b
waris0129 <waris0129@hotmail.com>
2022-07-31 22:35:00 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.24.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "nickname-tf-run"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+  credentials = "keys.json"
+}
+
+
+resource "google_cloud_run_service" "nickname-run-tf-version" {
+  
+    name     = "tf-v1"
+    location = "us-central1"
+    template {
+      spec {
+            containers {
+                image = "gcr.io/nickname-tf-run/test:try"
+            }
+      }
+    }
+
+
+}
+
+
+resource "google_cloud_run_service_iam_policy" "pub_access" {
+  service     = google_cloud_run_service.nickname-run-tf-version.name
+  location    = google_cloud_run_service.nickname-run-tf-version.location
+  policy_data = data.google_iam_policy.pub-1.policy_data
+}
+
+data "google_iam_policy" "pub-1" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers", ]
+  }
+}
+
